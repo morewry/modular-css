7b356823ec2a296c5dfaecbd8e6a8f5400e31cd0
Pat Cavit <github@patcavit.com>
2017-11-26 23:45:02 -0800

    build: clean up debug/release checking

--- packages/www/webpack.config.js
+++ packages/www/webpack.config.js
@@ -10,80 +10,85 @@ var path = require("path"),
     
     CSS = require("modular-css-webpack/plugin");
 
-module.exports = (env) => ({
-    entry   : "./src/index.js",
-    devtool : "cheap-source-map",
-    output  : {
-        path     : path.resolve("./dist"),
-        filename : "app.js"
-    },
-    
-    module : {
-        rules : [
-            {
-                test : /\.css$/,
-                use  : {
-                    loader  : "modular-css-webpack/loader",
-                    options : {
-                        namedExports : false
+module.exports = (env) => {
+    const release = env === "dist";
+    const debug   = !release;
+
+    return {
+        entry   : "./src/index.js",
+        devtool : "cheap-source-map",
+        output  : {
+            path     : path.resolve("./dist"),
+            filename : "app.js"
+        },
+        
+        module : {
+            rules : [
+                {
+                    test : /\.css$/,
+                    use  : {
+                        loader  : "modular-css-webpack/loader",
+                        options : {
+                            namedExports : false
+                        }
                     }
+                },
+                {
+                    test : /\.js$/,
+                    use  : "buble-loader"
                 }
-            },
-            {
-                test : /\.js$/,
-                use  : "buble-loader"
-            }
-        ]
-    },
-    
-    plugins : [
-        new Cleanup({
-            exclude : [
-                ".gitignore"
             ]
-        }),
+        },
+        
+        plugins : [
+            new Cleanup({
+                exclude : [
+                    ".gitignore"
+                ]
+            }),
 
-        // tree-shake ES modules
-        new webpack.optimize.ModuleConcatenationPlugin(),
+            // tree-shake ES modules
+            new webpack.optimize.ModuleConcatenationPlugin(),
 
-        // tree-shake commonJS modules
-        new Shake(),
+            // tree-shake commonJS modules
+            new Shake(),
 
-        new CSS({
-            css : "./app.css",
-            
-            namer : env === "dist" ?
-                 require("modular-css-namer")() :
-                 null,
-            
-            done : env === "dist" ?
-                // can't use cssnano until v4 is out :(
-                // [ require("cssnano")() ] :
-                [] : []
-        }),
+            new CSS({
+                css : "./app.css",
+                
+                namer : release ?
+                    require("modular-css-namer")() :
+                    null,
+                
+                done : release ?
+                    // can't use cssnano until v4 is out :(
+                    // [ require("cssnano")() ] :
+                    [] : []
+            }),
 
-        new HTML({
-            template : "./src/index.ejs",
-            inject   : true,
-            hash     : true,
-            title    : env === "dist" ?
-                "modular-css" : "modular-css local dev"
-        })
-    ],
-    
-    resolve : {
-        alias : {
-            fs     : require.resolve("./stubs/fs.js"),
-            module : require.resolve("./stubs/generic.js")
+            new HTML({
+                template : "./src/index.ejs",
+                inject   : true,
+                hash     : release,
+                title    : release ?
+                    "modular-css" : "modular-css local dev"
+            })
+        ],
+        
+        resolve : {
+            alias : {
+                fs     : require.resolve("./stubs/fs.js"),
+                module : require.resolve("./stubs/generic.js")
+            }
+        },
+
+        watchOptions : {
+            ignored : /node_modules/
+        },
+
+        devServer : {
+            compress   : true,
+            publicPath : "http://localhost:8080/"
         }
-    },
-
-    watchOptions : {
-        ignored : /node_modules/
-    },
-
-    devServer : {
-        compress   : true,
-        publicPath : "http://localhost:8080/"
-    }
-});
+    };
+};
